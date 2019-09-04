0ad847639d7236ae539f475a6b9efd3ccc91dd9e
Antonina Uskova <auskova@google.com>
2019-09-04 14:44:04 +0000

    Fixes and refactoring

--- timesketch/lib/analyzers/expert_sessionizers_test.py
+++ timesketch/lib/analyzers/expert_sessionizers_test.py
@@ -8,9 +8,10 @@ import mock
 from timesketch.lib.analyzers.expert_sessionizers import \
     WebActivitySessionizerSketchPlugin
 from timesketch.lib.analyzers.base_sessionizer_test import BaseSessionizerTest
-from timesketch.lib.analyzers.base_sessionizer_test import _create_mock_event
+from timesketch.lib.analyzers.base_sessionizer_test import _set_database
 from timesketch.lib.testlib import BaseTest
 from timesketch.lib.testlib import MockDataStore
+
 
 class TestWebActivitySessionizerPlugin(BaseTest, BaseSessionizerTest):
     """Tests the functionality of the web activity sessionizing sketch
@@ -22,30 +23,27 @@ class TestWebActivitySessionizerPlugin(BaseTest, BaseSessionizerTest):
     def test_event_type(self):
         """Test the mocking of events returns an event stream that matches the
         query for the analyzer."""
-        with mock.patch.object(
-                self.analyzer_class,
-                'event_stream',
-                return_value=_create_mock_event(
-                    0,
-                    2,
-                    source_attrs={'source_short': 'WEBHIST'})):
-            index = 'test_index'
-            sketch_id = 1
-            analyzer = self.analyzer_class(index, sketch_id)
-            message = analyzer.run()
-            self.assertEqual(
-                message,
-                'Sessionizing completed, number of session created: 1')
+        index = 'test_index'
+        sketch_id = 1
+        analyzer = self.analyzer_class(index, sketch_id)
+        analyzer.datastore.client = mock.Mock()
+        ds = analyzer.datastore
 
-            ds = MockDataStore('test', 0)
-            event1 = (ds.get_event('test_index', '0', stored_events=True))
-            self.assertEqual(event1['_source']['source_short'], 'WEBHIST')
-            self.assertEqual(event1['_source']['session_id'],
-                             {analyzer.session_type: 1})
-            event2 = (ds.get_event('test_index', '101', stored_events=True))
-            self.assertEqual(event2['_source']['source_short'], 'WEBHIST')
-            self.assertEqual(event2['_source']['session_id'],
-                             {analyzer.session_type: 1})
+        _set_database(ds, 0, 2, source_attrs={'source_short': 'WEBHIST'})
+
+        message = analyzer.run()
+        self.assertEqual(
+            message, 'Sessionizing completed, number of session created: 1')
+
+        event1 = (ds.get_event('test_index', '0', stored_events=True))
+        self.assertEqual(event1['_source']['source_short'], 'WEBHIST')
+        self.assertEqual(event1['_source']['session_id'],
+                         {analyzer.session_type: 1})
+        event2 = (ds.get_event('test_index', '101', stored_events=True))
+        self.assertEqual(event2['_source']['source_short'], 'WEBHIST')
+        self.assertEqual(event2['_source']['session_id'],
+                         {analyzer.session_type: 1})
+
 
 if __name__ == '__main__':
     unittest.main()
